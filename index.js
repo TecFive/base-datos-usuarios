c49bb41453d0b1e936f035a523a97aabc30e6447
Patricio <patovw@gmail.com>
2024-05-14 16:57:41 -0600

    update

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require('cors')
+
 const usersRouter = require('./controllers/user_controller')
 const loginRouter = require('./controllers/login_controller')
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json());
-app.use(cors())
 
 app.use('/login', loginRouter)
 app.use('/api/users', usersRouter)
